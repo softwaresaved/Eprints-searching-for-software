94ec6ad398cc2e34202d6c9aa29db494bc633560
steve-crouch <s.crouch@software.ac.uk>
2018-04-06 14:04:27 +0100

    Modularise yearly analysis, add in preliminary funder analysis

--- eprints_analyse.py
+++ eprints_analyse.py
@@ -24,6 +24,15 @@ OUTPUT_CSV_DIR = 'analysis_output/csv'
 OUTPUT_PNG_DIR = 'analysis_output/png'
 
 SUBSET_YEARS = [str(e) for e in list(range(2000,2017+1))]
+SUBSET_FUNDERS = {
+    'Engineering and Physical Sciences Research Council': 'EPSRC',
+    'Biotechnology and Biological Sciences Research Council': 'BBSRC',
+    'Economic and Social Research Council': 'ESRC',
+    'Natural Environment Research Council': 'NERC',
+    'Arts and Humanities Research Council': 'AHRC',
+    'Science and Technology Facilities Council': 'STFC',
+    'Medical Research Council': 'MRC',
+}
 
 
 def import_csv_to_df(filename):
@@ -37,7 +46,25 @@ def import_csv_to_df(filename):
 
 
 def clean_data(df):
+    def clean_funder(input_funder):
+        if not isinstance(input_funder, str):
+            return ''
+
+        for funder_full, funder_short in SUBSET_FUNDERS.items():
+            if funder_full in input_funder or funder_short in input_funder:
+                return funder_short
+
+    # Convert date to a date-friendly format
     df['date'] = pd.to_datetime(df['date'], dayfirst=True, errors='coerce')
+
+    # Add column just for year where we can
+    df['date_year'] = df['date'].dt.strftime('%Y')
+
+    # Get subset of years that we want
+    df = df[df['date_year'].isin(SUBSET_YEARS)]
+
+    # Convert funders to shortened names
+    df['funder'] = df['funder'].map(lambda x: clean_funder(x))
 
     return df
 
@@ -73,7 +100,7 @@ def save_bar_chart(df, x_col, y_col, file, percentage=False):
     fig.savefig(os.path.join(OUTPUT_PNG_DIR, file + '.png'))
 
 
-def count_unique_occurrences(df, count_col, file):
+def count_unique_occurrences(df, count_col):
     col = df[count_col]
 
     # Generate our count of each unique string
@@ -87,12 +114,6 @@ def count_unique_occurrences(df, count_col, file):
     cdf = cdf.sort_index()
 
     return cdf
-
-
-def add_extracted_year (df):
-    df['date_year'] = df['date'].dt.strftime('%Y')
-
-    return df
 
 
 def create_all_artefact_count():
@@ -111,30 +132,12 @@ def create_all_artefact_count():
     return df
 
 
-def main():
-
-    repo_info_df = import_csv_to_df(INPUT_REPOINFO_FILE)
-
-    # Import the overall eprints software term search metadata
-    # and convert date to a date-friendly format
-    df = import_csv_to_df(INPUT_METADATA_FILE)
-    df = clean_data(df)
-
-    # Merge all total repo artefact counts across all repositories
-    yearly_repo_info_df = create_all_artefact_count()
-
-    # Add column just for year where we can
-    with_year_df = add_extracted_year(df)
-
-    # Count artifacts by year, extract only those years we want
-    yearly_df = count_unique_occurrences(with_year_df, 'date_year', 'artifacts_by_year')
-
-    # Get subset of years that we want
-    # this also drops NaT conversions
-    yearly_df = yearly_df.loc[SUBSET_YEARS,:]
+def generate_yearly_software_percentages(df, yearly_repo_info_df):
+    # Count artifacts by year, ensure year index is integer for merging
+    yearly_df = count_unique_occurrences(df, 'date_year')
     yearly_df.index = yearly_df.index.map(int)
 
-    # Extract total sums across all repos, put into our high-level
+    # Extract total sums across all repos, merge into our high-level
     # dataframe and use to calculate overall percentages per year
     yearly_df['total_count'] = yearly_repo_info_df['total_count']
     yearly_df['percentage'] = round((yearly_df['count'] / yearly_df['total_count']) * 100, 1)
@@ -145,10 +148,42 @@ def main():
     chart_df = yearly_df['percentage']
     save_bar_chart(chart_df, '% EPrints software-related artifacts', 'Year', 'artifacts_by_year', percentage=True)
 
+
+def generate_funder_software_percentages(df, yearly_repo_info_df):
+    # Get subset of funders that we want
+    funder_df = df[df['funder'].isin(SUBSET_FUNDERS.values())]
+
+    # Count artifacts by funder
+    funder_df = count_unique_occurrences(funder_df, 'funder')
+    funder_df = funder_df.sort_values(by=['count'])
+
+    export_to_csv(funder_df, os.path.join(OUTPUT_CSV_DIR, 'artifacts_by_funder'))
+
+    # Generate and save a basic bar chart
+    save_bar_chart(funder_df, '# EPrints software-related artifacts', 'Funder', 'artifacts_by_funder', percentage=False)
+
+
+def main():
+
+    repo_info_df = import_csv_to_df(INPUT_REPOINFO_FILE)
+
+    # Import the overall eprints software term search metadata and clean it
+    df = import_csv_to_df(INPUT_METADATA_FILE)
+    df = clean_data(df)
+
+    # Merge all total repo artefact counts across all repositories
+    yearly_repo_info_df = create_all_artefact_count()
+
+    # Software-related by year analysis csv and chart
+    generate_yearly_software_percentages(df, yearly_repo_info_df)
+
+    # Software-related by funder analysis csv and chart
+    generate_funder_software_percentages(df, yearly_repo_info_df)
+
     num_total_records = yearly_repo_info_df['total_count'].sum()
-    num_software_records = len(with_year_df)
+    num_software_records = len(df)
     print("\nTotal number of records:", num_total_records)
-    print("Total number of valid software-related records:", len(with_year_df))
+    print("Total number of valid software-related records:", num_software_records)
     print("Overall percentage of software-related records;", round((num_software_records/num_total_records)*100, 1))
 
 
